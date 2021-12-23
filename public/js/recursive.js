36d152093b0e7da8e71a92ffdbf9c777379b2fc9
Carmine <carmine112255@gmail.com>
2021-12-22 20:24:55 -0600

    Dia 4 reestructurado

--- public/js/recursive.js
+++ public/js/recursive.js
@@ -2,7 +2,19 @@
 function startRecursive(){
     var count = document.getElementById('count').value;
     var word = document.getElementById('word').value;
-    repeat(count, word);
+    count = count ? count : null;
+    word = word ? word : null;
+
+    console.log(count);
+    console.log(word);
+
+    if(isEmpty(count) || isEmpty(word)){
+        console.log("Los campos no pueden estar vacios");
+        document.getElementById('result').innerHTML="Todos los campos deben de estar llenos";
+    }else{
+        document.getElementById('result').innerHTML="";
+        repeat(count, word);
+    }
 }
 
 function repeat(count, word){
@@ -15,3 +27,11 @@ function repeat(count, word){
         document.getElementById('result').insertAdjacentHTML("afterbegin", word + " ");
     }
 }
+
+function isEmpty(data){
+    if(data == null){
+        return true;
+    }else{
+        return false;
+    }
+};
--- resources/views/day4/index.blade.php
+++ resources/views/day4/index.blade.php
@@ -1,37 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Dia 4</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
+@extends('layout')
+@section('content')
+    <main class="container mx-auto flex flex-col items-center justify-center">
+        <x-page-header challengeNumber="4" challengeName="Repite la palabra"></x-page-header>
+        <section class="w-full mx-auto bg-ash-gray rounded-xl shadow-lg flex flex-col items-center py-10">
+            <form class="flex flex-col text-2xl p-1 mb-6">
+                <div class="flex justify-evenly my-6">
+                    <label for="word">Palabra</label>
+                    <input type="text" class="border-b-2 px-2 border-ebony bg-ash-gray" id="word" name="word">
+                </div>
 
-    <body>
-        <div class="container flex flex-col">
-            <main class="mx-auto border border-black p-5 w-9/12" >
-                <h2 class="text-center font-bold text-6xl mb-6">Reto 4</h2>
-                <h2 class="text-center font-bold text-5xl mb-6">Repite la palabra</h2>
-                <form class="flex flex-col text-2xl px-4 mb-6">
-                    <div class="flex justify-evenly my-6">
-                      <label for="word">Palabra</label>
-                      <input type="text" class="border-b-2 rounded-br-lg w-9/12 px-4 border-black" id="word" name="word">
-                    </div>
+                <div class="flex justify-evenly my-6">
+                    <label for="count" class=""># de repeticiones</label>
+                    <input type="number" class="border-b-2 px-2 border-ebony bg-ash-gray" id="count" name="count">
+                </div>
 
-                    <div class="flex justify-evenly my-6">
-                      <label for="count" class=""># de repeticiones</label>
-                      <input type="number" class="border-b-2 rounded-br-lg w-9/12 px-4 border-black" id="count" name="count">
-                    </div>
+                <x-simple-button text="Repetir" event="startRecursive()"></x-simple-button>
+            </form>
 
-                    <button type="button" class="mx-auto px-6 py-2 bg-blue-500 text-white rounded-md max-w-max shadow-xl hover:bg-blue-700 font-bold" onclick="startRecursive()">Repetir</button>
-                </form>
+            <h2 class="text-center px-16 py-4" id="result"></h2>
+            <x-return-button />
+        </section>
 
-                  <h2 class="text-center text-2xl" id="result"></h2>
-
-                <x-return-button />
-            </main>
-
-        </div>
-    </body>
+    </main>
     <script src="{{ asset('js/recursive.js') }}"></script>
-</html>
+@endsection
